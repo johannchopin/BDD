92bf6ef133d6e3363c74079a80ecc252ba091128
lyokolux <lyokolux@protonmail.com>
2018-05-03 14:18:06 +0200

    Fix a NameError when article's name have a slash

--- slurp_normal_articles.py
+++ slurp_normal_articles.py
@@ -1,3 +1,4 @@
+import threading
 from sys import exit, argv
 from os import mkdir, listdir
 from datetime import datetime 
@@ -5,7 +6,8 @@ from urllib.request import unquote
 try:
     import wikipedia
 except ImportError:
-    print('Please install the wikipedia\'s API')
+    print('Please install the wikipedia\'s API for python\n$ pip install wikipedia')
+    exit()
 
 # ===========
 # initization 
@@ -22,22 +24,31 @@ downloaded = set([e.lower() for e in listdir('articles/normal')]).union(ADQ).uni
 # bound equal to the parameter, else 100 000
 bound = int(argv[1]) if len(argv) > 1 else 100000
 
+my_lock = threading.RLock()
+
 # ====
 # core 
 # ====
 
-while len(downloaded) <= bound:
+def download_articles():
+    while len(downloaded) <= bound:
+        try:
+            # Get the title of the first reference in wikipedia, avoid some DisambiguationError
+            title = wikipedia.search(wikipedia.random(), results=1)[0]
+            article = wikipedia.WikipediaPage(title).html()
+        except wikipedia.exceptions.DisambiguationError:
+            print('DisambiguationError')
+        except:
+            print('Unknow error')
+        else:
+            if title.lower() not in downloaded:
+                with my_lock:
+                    try:
+                        with open("articles/normal/" + title, 'x') as f:
+                            f.write(article)
+                    except:
+                        continue
+                    downloaded.add(title.lower())
+                    print(f"{title}")
 
-    try:
-        # Get the title of the first reference in wikipedia, avoid some DisambiguationError
-        title = wikipedia.search(wikipedia.random(), results=1)[0]
-        article = wikipedia.WikipediaPage(title).html()
-    except wikipedia.exceptions.DisambiguationError:
-        print('DisambiguationError')
-    else:
-        title = title.replace('\\', ' ')
-        if title.lower() not in downloaded:
-            with open("articles/normal/" + title, 'x') as f:
-                f.write(article)
-            downloaded.add(title.lower())
-            print(f"{title}")
+threading.Thread(target=download_articles()).start()
