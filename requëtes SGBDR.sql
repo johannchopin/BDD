49ea1334a45e10e2c06272aca07c787812938ea0
johannchopin <31794680+johannchopin@users.noreply.github.com>
2018-05-28 16:41:20 +0200

    Update requëtes SGBDR.sql

--- requëtes SGBDR.sql
+++ requëtes SGBDR.sql
@@ -1,8 +1,3 @@
-CREATE TABLE lien(id_lien integer AUTO_INCREMENT PRIMARY KEY,
-	  nom varchar(50),
-	  url varchar(50)
-);
-
 CREATE TABLE contributeur(id_contributeur integer AUTO_INCREMENT,
 	  nom varchar(50),
 	  date_inscription date,
@@ -19,10 +14,8 @@ CREATE TABLE article(id_article integer AUTO_INCREMENT,
                      nb_langues integer,
                      nb_sources integer,
                      date_modif date,
-	  id_lien integer,
 	  id_contributeur integer,
 PRIMARY KEY (id_article),
-FOREIGN KEY (id_lien) REFERENCES lien(id_lien),
 FOREIGN KEY(id_contributeur) REFERENCES contributeur(id_contributeur)
 );
 
@@ -35,6 +28,13 @@ CREATE TABLE categorie(id_categorie integer AUTO_INCREMENT PRIMARY KEY,
 	  nom varchar(50),
 	  id_portail integer,
 FOREIGN KEY (id_portail) REFERENCES portail(id_portail)
+);
+
+CREATE TABLE categorie_portail(id_categorie integer,
+		id_portail integer,
+PRIMARY KEY(id_categorie, id_portail),
+FOREIGN KEY(id_categorie) REFERENCES categorie(id_categorie),
+FOREIGN KEY(id_portail) REFERENCES portail(id_portail)
 );
 
 CREATE TABLE categories_article(id_article integer,
