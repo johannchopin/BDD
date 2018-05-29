afbbdbeb2d8ffca124cf90408983592f9477b3c5
Justin-HENRY <31780502+Justin-HENRY@users.noreply.github.com>
2018-05-29 15:49:25 +0200

    Update requëtes SGBDR.sql

--- requëtes SGBDR.sql
+++ requëtes SGBDR.sql
@@ -1,4 +1,4 @@
-CREATE TABLE contributeur(id_contributeur integer AUTO_INCREMENT,
+CREATE TABLE contributeur(id_contributeur varchar(20),
 	  nom varchar(50),
 	  date_inscription date,
 	  nb_modification integer,
@@ -6,7 +6,7 @@ CREATE TABLE contributeur(id_contributeur integer AUTO_INCREMENT,
 PRIMARY KEY(id_contributeur)
 );
 
-CREATE TABLE article(id_article integer AUTO_INCREMENT,
+CREATE TABLE article(id_article varchar(20),
                      nom varchar(50),
                      type varchar(50),
                      nb_parties integer,
@@ -14,42 +14,39 @@ CREATE TABLE article(id_article integer AUTO_INCREMENT,
                      nb_langues integer,
                      nb_sources integer,
                      date_modif date,
-	  id_contributeur integer,
+	  id_contributeur varchar(20),
 PRIMARY KEY (id_article),
 FOREIGN KEY(id_contributeur) REFERENCES contributeur(id_contributeur)
 );
 
-CREATE TABLE portail(id_portail integer AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE portail(id_portail varchar(20) PRIMARY KEY,
 	  nom varchar(50),
 	  nb_articles integer
 );
 
-CREATE TABLE categorie(id_categorie integer AUTO_INCREMENT PRIMARY KEY,
-	  nom varchar(50),
-	  id_portail integer,
-FOREIGN KEY (id_portail) REFERENCES portail(id_portail)
+CREATE TABLE categorie(id_categorie varchar(20) PRIMARY KEY,
+	  nom varchar(50)
 );
 
-CREATE TABLE categorie_portail(id_categorie integer,
-		id_portail integer,
+CREATE TABLE categorie_portail(id_categorie varchar(20),
+		id_portail varchar(20),
 PRIMARY KEY(id_categorie, id_portail),
 FOREIGN KEY(id_categorie) REFERENCES categorie(id_categorie),
 FOREIGN KEY(id_portail) REFERENCES portail(id_portail)
 );
 
-CREATE TABLE categories_article(id_article integer,
-	  id_categorie integer,
+CREATE TABLE categories_article(id_article varchar(20),
+	  id_categorie varchar(20),
 PRIMARY KEY (id_article, id_categorie),
 FOREIGN KEY(id_article) REFERENCES article(id_article),
 FOREIGN KEY(id_categorie) REFERENCES categorie(id_categorie)
 );
 
-CREATE TABLE bot(id_bot integer AUTO_INCREMENT,
+CREATE TABLE bot(id_bot varchar(20),
 	  nom varchar(50),
 	  date_creation date,
 	  nb_modification integer,
-      id_contributeur integer,
+      id_contributeur varchar(20),
 PRIMARY KEY(id_bot),
 FOREIGN KEY(id_contributeur) REFERENCES contributeur(id_contributeur)
 );
-
